e59a2fb20a1a39cb6293b50bd856596a71ab14cb
TomLBZ <libozhao142857@hotmail.com>
2022-04-20 20:29:17 +0800

    added char map, jumping test, img. changed readme.

---
+++ 8 jumping test/main.h
@@ -0,0 +1,46 @@
+#include <gb/gb.h>
+#include <gbdk/font.h>
+#include <stdio.h>
+
+#if !defined(MAIN_MACROS)
+#define MAIN_MACROS
+
+#define CHAR__  0x00
+#define CHAR_0  0x01
+#define CHAR_1  0x02
+#define CHAR_2  0x03
+#define CHAR_3  0x04
+#define CHAR_4  0x05
+#define CHAR_5  0x06
+#define CHAR_6  0x07
+#define CHAR_7  0x08
+#define CHAR_8  0x09
+#define CHAR_9  0x0A
+#define CHAR_A  0x0B
+#define CHAR_B  0x0C
+#define CHAR_C  0x0D
+#define CHAR_D  0x0E
+#define CHAR_E  0x0F
+#define CHAR_F  0x10
+#define CHAR_G  0x11
+#define CHAR_H  0x12
+#define CHAR_I  0x13
+#define CHAR_J  0x14
+#define CHAR_K  0x15
+#define CHAR_L  0x16
+#define CHAR_M  0x17
+#define CHAR_N  0x18
+#define CHAR_O  0x19
+#define CHAR_P  0x1A
+#define CHAR_Q  0x1B
+#define CHAR_R  0x1C
+#define CHAR_S  0x1D
+#define CHAR_T  0x1E
+#define CHAR_U  0x1F
+#define CHAR_V  0x20
+#define CHAR_W  0x21
+#define CHAR_X  0x22
+#define CHAR_Y  0x23
+#define CHAR_Z  0x24
+
+#endif // MAIN_MACROS
--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-# TouhouGBA ~ 东方寄必诶
-## What is this? ~ 这是啥？
+# TouhouGBA～東方寄必诶
+## What is this?～这是啥？
 This is a place where I attempt to learn making GBA games and ultimately create a Touhou GBA fangame.
 
 以制作东方同人游戏为目标尝试学习GBA游戏开发的代码。
-## Status ~ 状态
-Just Started
+## Status～状态
+Basic working GB file for testing purposes√
 
-刚开始
+可运行的测试用GB文件√
+## Screenshot～截图
+[!test.png](img/test.png)
+## Characters～自機紹介
+|Features<br/>特性|Hakurei Reimu<br/>博麗霊夢|Kirisame Marisa<br/>霧雨魔理沙|
+|--------|-------------|---------------|
+|Abililty<br/>能力|Unaffected by gravity<br/>在天空飞行程度的能力|Using magic<br/>使用魔法程度的能力|
+|Effect<br/>效果|Moving speed is uniform in all 4 directions, does not fall down even when not actively flying<br/>四方向移动速度相同，不会从空中掉落|Falls down slowly if not actively flying, but can accelerate upwards / downwards significantly faster<br/>不主动飞行时会从空中缓缓掉落，但上/下加速很快|
+|Bomb<br/>符卡|Spirit sign "Dream Seal"<br/>霊符「夢想封印」|Love sign "Master Spark"<br/>恋符「マスタースパーク」|
